43e707794736fe80cf73e2184fff5279bb21d97a
Nayem <nayemk900@gmail.com>
2021-04-29 11:44:51 +0600

    Appointment btn

--- src/component/HomePage/Services/ServiceDetails.js
+++ src/component/HomePage/Services/ServiceDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ServiceDetails = ({ service }) => {
     return (
@@ -6,6 +7,7 @@ const ServiceDetails = ({ service }) => {
                 <img  style={{width: '60px'}} src={service.img} alt=""/>
                 <h6 className='mt-3 mb-2'>{service.name}</h6>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, ducimus.</p>
+                <Link to='/appointment' className="btn btn-primary">APPOINTMENT</Link>
         </div>
     );
 };
